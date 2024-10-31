0388f9040233000d7bcf5d9c94a6c9f86b5827ae
Mortalium <152898932+Mortalium@users.noreply.github.com>
2024-10-31 12:14:48 +0100

    added express proxy to vue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,5 @@
 import express from ("express")
 const app = express()
-app.get()
+
+
+app.listen(3000)
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
   plugins: [
     vue(),
   ],

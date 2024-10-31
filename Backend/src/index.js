dc92009049f60e7210713aaf57e47f6e345ab4c8
Roadraudi <152898759+Roadraudi@users.noreply.github.com>
2024-10-31 15:40:21 +0100

    backend logic

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const db = new sqlite3.Database("../res/game_data.db");
+
 let sessions = {}
 
 wss.on('connection', (ws, req) => {
@@ -21,12 +23,29 @@ wss.on('connection', (ws, req) => {
         }
        
       } else if (data.type === "new") {
-        sessionId = 
+        sessionId = this.startGame()
         sessions[sessionId] = [];
         sessions[sessionId].push(ws);
 
       } else if (data.type === "update") {
         const sessionId = data.sessionId;
+        this.updateGame(data.board, sessionId);
+        winnerVar = this.checkWinner(data.board);
+        if(winnerVar != null){
+            ws.send(JSON.stringify({type: 'win'}));
+
+            sessions[sessionId].forEach(client => {
+                if (client !== ws) {
+                    client.send(JSON.stringify({ type: 'loss'}));
+                }
+            });
+        }else{
+            if(this.isBoardFull){
+                sessions[sessionId].forEach(client => {
+                    client.send(JSON.stringify({ type: 'even'}));
+                });
+            }
+        }
 
         sessions[sessionId].forEach(client => {
             if (client !== ws) {
@@ -36,6 +55,54 @@ wss.on('connection', (ws, req) => {
       }
     });
   });
+
+exports.startGame = () => {
+    db.run('INSERT INTO data_sets (data_json) VALUES (?)',[""], function(err){
+        if(err){
+            return console.log('Fehler beim Hinzufügen:', err.message);
+        }
+        return this.lastID;
+    });
+};
+
+exports.updateGame = (req, sessionID) => {
+    db.run('UPDATE data_sets SET data_json = ? WHERE sessionID = ?', [req, sessionID]);
+};
+
+exports.checkWinner = ( board) => {
+    for (let row of Object.values(board)) {
+        if (row[0] === row[1] && row[1] === row[2] && row[0] !== "") {
+            return row[0]; // Gewinner gefunden
+        }
+    }
+
+    // Überprüfen der Spalten
+    for (let col = 0; col < 3; col++) {
+        if (board.a[col] === board.b[col] && board.b[col] === board.c[col] && board.a[col] !== "") {
+            return board.a[col]; // Gewinner gefunden
+        }
+    }
+
+    // Überprüfen der Diagonalen
+    if (board.a[0] === board.b[1] && board.b[1] === board.c[2] && board.a[0] !== "") {
+        return board.a[0]; // Gewinner gefunden
+    }
+
+    if (board.a[2] === board.b[1] && board.b[1] === board.c[0] && board.a[2] !== "") {
+        return board.a[2]; // Gewinner gefunden
+    }
+
+    return null;
+}
+
+exports.isBoardFull = (board) => {
+    for (let row of Object.values(board)) {
+        if (row[0] == "" || row[1] == "" || row[2] == "") {
+            return false;
+        }
+        return true;
+    }
+}
 
 app.get("/api/", (req, res) => {
     res.send("Hello express world");

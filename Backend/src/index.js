7e2b6f35bb4d7ce1173222ebc683bc42d7964140
Mortalium <152898932+Mortalium@users.noreply.github.com>
2024-10-31 13:49:29 +0100

    websocket logic

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,41 @@
 const express = require("express");
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let sessions = {}
+
+wss.on('connection', (ws, req) => {
+    ws.on('message', (message) => {
+      const data = JSON.parse(message);
+      if (data.type === 'join') {
+        if (sessions[sessionId] != null){
+            
+            const sessionId = data.sessionId;
+
+            sessions[sessionId].push(ws);
+    
+        }
+       
+      } else if (data.type === "new") {
+        sessionId = 
+        sessions[sessionId] = [];
+        sessions[sessionId].push(ws);
+
+      } else if (data.type === "update") {
+        const sessionId = data.sessionId;
+
+        sessions[sessionId].forEach(client => {
+            if (client !== ws) {
+                client.send(JSON.stringify({ type: 'update', message: 'Player joined' }));
+            }
+        });
+      }
+    });
+  });
 
 app.get("/api/", (req, res) => {
     res.send("Hello express world");
